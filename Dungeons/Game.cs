b126c802edc37a8c58269da7e3d8602d247d8282
feylikurds <feylikurds@gmail.com>
2016-04-04 01:20:36 +0200

    Added GPL.

--- Dungeons/Game.cs
+++ Dungeons/Game.cs
@@ -1,4 +1,22 @@
-﻿using System;
+/*
+Dungeons, a C# 6.0 coding example in form of a console game.
+Copyright (C) 2016 Aryo Pehlewan aryopehlewan@hotmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
